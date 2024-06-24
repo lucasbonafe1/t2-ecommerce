d6d00e1c644aadad9ffdd6473d3a578e88df83d5
Miguel Caldas <miguelcaldas05@hotmail.com>
2024-06-24 16:53:56 -0300

    Style: homePage

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,13 +1,29 @@
-import { View, Text } from 'react-native'
-import styles from '../home/style'
-import React from 'react'
+import { View, Text } from 'react-native';
+import styles from '../home/style';
+import React from 'react';
+
+import * as Animatable from 'react-native-animatable';
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>PAGINA HOME DE CRIA</Text>
+      <View style={styles.containerLogo}>
+        <Animatable.Image
+          animation="flipInY"
+          source={require("../../../assets/logo/logo.png")}
+          style={{ width: '15%' }}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.produtosTextContainer}>
+        <Text style={styles.text}>Produtos</Text>
+      </View>
+      <View style={styles.line} />
+      <View style={styles.adicionadosTextContainer}>
+        <Text style={styles.text}>Adicionados recentemente</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default Home;
--- src/screens/home/style.ts
+++ src/screens/home/style.ts
@@ -1,11 +1,38 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
-        alignItems:'center',
-        justifyContent: 'center'
-    }
+        backgroundColor: '#232323',
+        justifyContent: 'flex-start',
+    },
+    containerLogo: {
+        flex: 0.18,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    produtosTextContainer: {
+        flex: 0.15,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    adicionadosTextContainer: {
+        flex: 0.4,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    line: {
+        borderBottomColor: '#FFFFFF',
+        borderBottomWidth: 1,
+        width: '80%',
+        alignSelf: 'center',
+        marginVertical: 10,
+    },
+    text: {
+        color: '#FFFFFF',
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
 });
 
 export default styles;
