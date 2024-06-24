956c0b03c346e69a079a27426fdf2f7fcc0ad2c1
Miguel Caldas <miguelcaldas05@hotmail.com>
2024-06-24 15:11:29 -0300

    Feat: cadastroProduto

---
+++ src/screens/cadastroProduto/index.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { View, TextInput, Text, TouchableOpacity } from "react-native";
+import styles from "./style";
+
+export function Screen() {
+    const [titulo, setTitulo] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [preco, setPreco] = useState("");
+
+    const handlePress = () => {
+        console.log("Título:", titulo);
+        console.log("Descrição:", descricao);
+        console.log("Preço:", preco);
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.quad} />
+            <View style={[styles.quad2, { justifyContent: 'flex-start', paddingTop: 20 }]}>
+                <Text style={[styles.titlePag, { fontWeight: 'bold' }]}>Novo Produto</Text>
+                <View style={styles.exportQuad} />
+            </View>
+            <View style={[styles.quad3, { backgroundColor: '#232323' }]}>
+                <Text style={styles.title}>Título</Text>
+                <TextInput
+                    placeholder="Digite o Título do Produto"
+                    placeholderTextColor="#909094"
+                    style={[styles.input, { backgroundColor: '#D9D9D9', color: '#000' }]}
+                    onChangeText={setTitulo}
+                    value={titulo}
+                />
+            </View>
+            <View style={[styles.quad4, { backgroundColor: '#232323' }]}>
+                <Text style={styles.title}>Descrição</Text>
+                <TextInput
+                    placeholder="Digite a Descrição do Produto"
+                    placeholderTextColor="#909094"
+                    style={[styles.input, { backgroundColor: '#D9D9D9', color: '#000' }]}
+                    onChangeText={setDescricao}
+                    value={descricao}
+                />
+            </View>
+            <View style={[styles.quad5, { backgroundColor: '#232323' }]}>
+                <Text style={styles.title}>Preço</Text>
+                <TextInput
+                    placeholder="Digite o Preço do Produto"
+                    placeholderTextColor="#909094"
+                    style={[styles.input, { backgroundColor: '#D9D9D9', color: '#000' }]}
+                    onChangeText={setPreco}
+                    value={preco}
+                />
+            </View>
+            <View style={styles.quad6} />
+            <View style={[styles.quad7, { flex: 2, backgroundColor: '#232323' }]}>
+                <TouchableOpacity style={styles.button} onPress={handlePress}>
+                    <Text style={styles.buttonText}>Enviar</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.quad8} />
+        </View>
+    );
+}
+
+export default Screen;
---
+++ src/screens/cadastroProduto/style.ts
@@ -0,0 +1,90 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#232323",
+    },
+    quad: {
+        backgroundColor: "#232323",
+        flex: 1,
+    },
+    quad2: {
+        backgroundColor: "#232323",
+        flex: 7,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    titlePag: {
+        color: "#FFF",
+        fontSize: 27,
+        marginBottom: 20,
+    },
+    exportQuad: {
+        backgroundColor: "#D9D9D9",
+        width: 160,
+        height: 160,
+        borderRadius: 10,
+    },
+    quad3: {
+        backgroundColor: "#232323",
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    quad4: {
+        backgroundColor: "#232323",
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    quad5: {
+        backgroundColor: "#232323",
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    quad6: {
+        backgroundColor: "#232323",
+        flex: 0.3,
+    },
+    quad7: {
+        backgroundColor: "#363A43",
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    quad8: {
+        backgroundColor: "#232323",
+        flex: 1,
+    },
+    title: {
+        color: "#FFF",
+        fontSize: 22,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "#FFF",
+        color: "#000", // Texto preto
+        padding: 10,
+        width: '85%',
+        height: 45,
+        borderRadius: 10,
+        fontSize: 20,
+    },
+    button: {
+        backgroundColor: "#E45858",
+        width: '75%',
+        borderRadius: 10,
+        paddingVertical: 8,
+        marginTop: 14,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "#FFF",
+        fontSize: 30,
+    },
+});
+
+export default styles;
