6671b7169033790c9d2550ef8f52a6f73b123405
Miguel Caldas <miguelcaldas05@hotmail.com>
2024-06-25 00:10:14 -0300

    Style: drawer

--- src/routes/drawer.tsx
+++ src/routes/drawer.tsx
@@ -3,6 +3,10 @@ import Welcome from '../screens/welcome';
 import Login from '../screens/login';
 import Home from '../screens/home';
 import { CadastroProduto } from '../screens/cadastroProduto';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome6 } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 type DrawerNavigation = {
     Welcome: undefined,
@@ -13,35 +17,60 @@ type DrawerNavigation = {
 const Drawer = createDrawerNavigator();
 export type DrawerTypes = DrawerNavigationProp<DrawerNavigation>;
 
-export default function DrawerRoutes(){
-    return(
-        <Drawer.Navigator>
+export default function DrawerRoutes() {
+    return (
+        <Drawer.Navigator
+            screenOptions={{
+                drawerActiveTintColor: '#EAEAEA',
+                drawerInactiveBackgroundColor: '#232323',
+                drawerInactiveTintColor: '#EAEAEA',
+                drawerActiveBackgroundColor: '#E45858',
+                drawerStyle: {
+                    backgroundColor: '#232323',
+                },
+                drawerLabelStyle: {
+                    fontSize: 19,
+                },
+            }}
+        >
             <Drawer.Screen
                 name='Home'
                 component={Home}
                 options={{
-                    headerShown: false
+                    headerShown: false,
+                    drawerIcon: ({ size }) => (
+                        <FontAwesome name="home" size={size} color={'#EAEAEA'} />
+                    ),
                 }}
             />
             <Drawer.Screen
                 name='Adicionar Produto'
                 component={CadastroProduto}
                 options={{
-                    headerShown: false
+                    headerShown: false,
+                    drawerIcon: ({ size }) => (
+                        <FontAwesome6 name="add" size={size} color={'#EAEAEA'} />
+                    ),
                 }}
             />
             <Drawer.Screen
                 name='Sobre'
                 component={Login}
                 options={{
-                    headerShown: false
+                    headerShown: false,
+                    drawerIcon: ({ size }) => (
+                        <Ionicons name="people-sharp" size={size} color={'#EAEAEA'} />
+                    ),
                 }}
             />
             <Drawer.Screen
                 name='Sair'
                 component={Welcome}
                 options={{
-                    headerShown: false
+                    headerShown: false,
+                    drawerIcon: ({ size }) => (
+                        <MaterialIcons name="logout" size={size} color={'#EAEAEA'} />
+                    ),
                 }}
             />
         </Drawer.Navigator>
